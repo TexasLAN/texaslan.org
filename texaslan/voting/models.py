b2ad8117f2aee31de5de42b2f5e3e21d6fc39102
hsmeans <spencer.means@gitty.ai>
2018-02-06 19:52:14 -0600

    Special Election

--- texaslan/voting/models.py
+++ texaslan/voting/models.py
@@ -10,13 +10,13 @@ from texaslan.site_settings.models import SiteSettingService
 
 CANDIDATE_POSITIONS = (
     # ("P", "President"),
-    ("A", "VP of Administration"),
+    # ("A", "VP of Administration"),
     # ("T", "Treasurer"),
     # ("S", "VP of Service"),
     # ("N", "VP of New Member Services"),
-    ("O", "VP of Social Affairs"),
+    # ("O", "VP of Social Affairs"),
     # ("J", "VP of Standards"),
-    # ("R", "Risk Management"),
+    ("R", "Risk Management"),
     # ("B", "Standards Board"),
 )
 
