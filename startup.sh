c9860873b3589c6dc507bd45bf4b78b434dd7b1d
Steven Diaz <stevendiaz@utexas.edu>
2018-02-20 11:30:28 -0600

    Run necessary migrations on startup

--- startup.sh
+++ startup.sh
@@ -6,5 +6,8 @@ fi
 whoami;
 su postgres -c "createdb texaslan";
 su postgres -c "python manage.py migrate";
+su postgres -c "python manage.py loaddata user_type_groups.json"
+su postgres -c "python manage.py loaddata tags.json"
+su postgres -c "python manage.py loaddata test_users.json"
 gulp;
 su postgres -c "python manage.py runserver 0.0.0.0:8000"
